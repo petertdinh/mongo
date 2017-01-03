8dd5c26d932863a5924d504b9bbd5cb2aac08bfc
Peter <peter.dinh@gmail.com>
2017-01-02 19:25:49 -0800

    first commit

---
+++ test/update_test.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Updating records', () => {
+	let joe;
+
+	beforeEach((done) => {
+		joe = new User({name: 'joe'});
+		joe.save()
+			.then(() => done());
+	});
+
+	function assertName(operation, done) {
+		operation
+		.then(() => User.find({}))
+		.then((users) => {
+					assert(users.length === 1);
+					assert(users[0].name === 'alex');
+					done();
+				})
+	};
+
+	it('instance type using set n save', (done) => {
+		joe.set('name', 'alex');
+		assertName(joe.save(), done);
+	});
+
+	it('a model instance can update', (done) => {
+		assertName(joe.update({name: 'alex'}), done);
+	});
+
+	it('a model class can update', (done) => {
+		assertName(
+			User.update({name: 'joe'}, {name: 'alex'}), done);
+	});
+
+	it('a model class can update one record', (done) => {
+		assertName(
+			User.findOneAndUpdate({name: 'joe'}, {name: 'alex'}), done);
+	});
+
+	it('a model class can find a record with an Id and update', (done) => {
+		assertName(
+			User.findByIdAndUpdate(joe._id, {name: 'alex'}), done);
+	});
+})
